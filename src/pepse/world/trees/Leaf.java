afcbe73c181855668c974ad9827fb24b7977e689
Ido Pinto <ido.pinto@mail.huji.ac.il>
2021-12-25 17:49:55 +0200

    feature leaf implemented before testing

--- src/pepse/world/trees/Leaf.java
+++ src/pepse/world/trees/Leaf.java
@@ -1,12 +1,21 @@
 package pepse.world.trees;
 
+import danogl.GameObject;
+import danogl.components.CoordinateSpace;
+import danogl.gui.rendering.RectangleRenderable;
+import danogl.gui.rendering.Renderable;
+import danogl.util.Vector2;
+import pepse.util.ColorSupplier;
+import pepse.world.Block;
+
 import java.awt.*;
 
 public class Leaf {
 
     private static final Color LEAF_COLOR =new Color(50, 200, 30);
 
-
-    //    Renderable renderable = new RectangleRenderable(ColorSupplier.approximateColor(LEAF_COLOR));
+    public Block create(Vector2 leafTopLeftCorner){
+        return new Block(leafTopLeftCorner, new RectangleRenderable(LEAF_COLOR));
+    }
 
 }
