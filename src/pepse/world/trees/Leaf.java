8cf8a32114e0744ade00d82d00dbdcb554be3e2b
Ido Pinto <ido.pinto@mail.huji.ac.il>
2021-12-27 16:12:47 +0200

    changing leaf to extend block

--- src/pepse/world/trees/Leaf.java
+++ src/pepse/world/trees/Leaf.java
@@ -12,29 +12,41 @@ import pepse.world.Block;
 import java.awt.*;
 import java.util.Random;
 
-public class Leaf {
+public class Leaf extends Block{
 
-    private static final Color LEAF_COLOR =new Color(50, 200, 30);
-    private static final String LEAF_TAG = "leaf";
-    private Block leaf;
+//    private static final Color LEAF_COLOR =new Color(50, 200, 30);
+//    private static final String LEAF_TAG = "leaf";
+    private static final int FADEOUT_TIME = 6;
+    private static final int FALL_VELOCITY = 20;
+
+    private int  randomLifeSpan;
 
 
-    public Block create(GameObjectCollection gameObjects, Vector2 leafTopLeftCorner, int layer){
-        Renderable renderable = new RectangleRenderable(ColorSupplier.approximateColor(LEAF_COLOR));
-        leaf = new Block(leafTopLeftCorner,renderable);
-        gameObjects.addGameObject(leaf,layer);
-        leaf.setTag(LEAF_TAG);
-        new ScheduledTask(leaf,new Random().nextFloat() ,true,this::makeItMove);
-        new ScheduledTask(leaf, new Random().nextInt(5), true, this::makeItFade);
-//        leaf.renderer().fadeOut(6);
-        return leaf;
+
+    /**
+     * Construct a new GameObject instance.
+     *
+     * @param topLeftCorner The location of the top-left corner of the created block
+     * @param renderable    - A renderable to render as the block.
+     */
+    public Leaf(Vector2 topLeftCorner, Renderable renderable) {
+        super(topLeftCorner, renderable);
+        leafRoutine();
     }
+
+//    public Block create(GameObjectCollection gameObjects, Vector2 leafTopLeftCorner, int layer){
+//        Renderable renderable = new RectangleRenderable(ColorSupplier.approximateColor(LEAF_COLOR));
+//        leaf = new Block(leafTopLeftCorner,renderable);
+//        gameObjects.addGameObject(leaf,layer);
+//        leaf.setTag(LEAF_TAG);
+//        return leaf;
+//    }
 
 
     private void makeItMove()
     {
-        new Transition<Float>(leaf,
-                leaf.renderer()::setRenderableAngle,
+        new Transition<Float>(this,
+                this.renderer()::setRenderableAngle,
                 0f,
                 50f,
                 Transition.LINEAR_INTERPOLATOR_FLOAT, // use a cubic interpolator
@@ -42,8 +54,8 @@ public class Leaf {
                 Transition.TransitionType.TRANSITION_BACK_AND_FORTH,
                 null); // nothing further to execute upon reaching final value
 
-        new Transition<Vector2>(leaf,
-                leaf::setDimensions,
+        new Transition<Vector2>(this,
+                this::setDimensions,
                 new Vector2(26,26),
                 new Vector2(30,30),
                 Transition.LINEAR_INTERPOLATOR_VECTOR, // use a cubic interpolator
@@ -51,15 +63,27 @@ public class Leaf {
                 Transition.TransitionType.TRANSITION_BACK_AND_FORTH,
                 null); // nothing further to execute upon reaching final value
 
-//        new Transition<Vector2>(leaf,
-//                leaf.renderer()::fadeOut,
-//                new
-//        )
+    }
+
+    public void leafRoutine()
+    {
+        new ScheduledTask(this,new Random().nextFloat() ,true,this::makeItMove);
+        new ScheduledTask(this, new Random().nextInt(5), false, this::leafFallRoutine);
+    }
+
+    private void leafFallRoutine()
+    {
+        makeItFall();
+        makeItFade();
     }
 
     private void makeItFade(){
-        leaf.renderer().fadeOut(6);
+        this.renderer().fadeOut(FADEOUT_TIME);
     }
 
+    private void makeItFall(){
+        this.transform().setVelocityY(Vector2.DOWN.y()*FALL_VELOCITY);
+
+    }
 
 }
--- src/pepse/world/trees/TreeTop.java
+++ src/pepse/world/trees/TreeTop.java
@@ -2,13 +2,19 @@ package pepse.world.trees;
 
 import danogl.collisions.GameObjectCollection;
 import danogl.components.ScheduledTask;
+import danogl.gui.rendering.RectangleRenderable;
+import danogl.gui.rendering.Renderable;
 import danogl.util.Vector2;
+import pepse.util.ColorSupplier;
 import pepse.world.Block;
 
+import java.awt.*;
 import java.util.Random;
 
 public class TreeTop {
 
+    private static final Color LEAF_COLOR =new Color(50, 200, 30);
+    private static final String LEAF_TAG = "leaf";
 
     static void createTreeTop(GameObjectCollection gameObjects, int layer, Vector2 topLeftCorner,int  leafInRow)
     {
@@ -17,8 +23,10 @@ public class TreeTop {
         for (int i = 0; i < leafInRow; i++) {
             x = (int) topLeftCorner.x();
             for (int j = 0; j < leafInRow; j++) {
-                Leaf leaf = new Leaf();
-                leaf.create(gameObjects,new Vector2(x,y),layer);
+                Renderable renderable = new RectangleRenderable(ColorSupplier.approximateColor(LEAF_COLOR));
+                Leaf leaf = new Leaf(new Vector2(x,y),renderable);
+                gameObjects.addGameObject(leaf,layer);
+                leaf.setTag(LEAF_TAG);
                 x += Block.SIZE;
             }
             y +=Block.SIZE;
